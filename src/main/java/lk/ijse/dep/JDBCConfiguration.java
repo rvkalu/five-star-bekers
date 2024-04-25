0b9c204af0fbe5edcedcb02d37adf2a6bef63fd2
rvkalu <rvkalupahana@gmail.com>
2024-04-25 12:59:22 +0530

    rv add

---
+++ src/main/java/lk/ijse/dep/JDBCConfiguration.java
@@ -0,0 +1,4 @@
+package lk.ijse.dep;
+
+public class JDBCConfiguration {
+}
--- src/main/java/lk/ijse/dep/Main.java
+++ src/main/java/lk/ijse/dep/Main.java
@@ -2,6 +2,7 @@ package lk.ijse.dep;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("rvkalu");
         System.out.println("Hello world!");
     }
 }
