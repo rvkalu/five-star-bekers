8c1e94f402fc971e840fcb70c26d6512f7047860
rvkalu <rvkalupahana@gmail.com>
2024-04-25 13:00:29 +0530

    rv add

--- src/main/java/lk/ijse/dep/Main.java
+++ src/main/java/lk/ijse/dep/Main.java
@@ -2,7 +2,7 @@ package lk.ijse.dep;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("rvkalu");
+        System.out.println("rvkalupahana");
         System.out.println("Hello world!");
     }
 }
